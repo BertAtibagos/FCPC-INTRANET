35a9363801f9b1ab66723be1d526fd5810fc48cb
BertAtibagos <atibagos.robert2300@gmail.com>
2026-01-21 16:53:43 +0800

    Imported fontawesome

--- src/components/sidemenu.jsx
+++ src/components/sidemenu.jsx
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
 const SideMenu = (props) => {
     const { classname } = props;
     return(
         <div className={ classname }>
             <button className="bg-neutral-50 drop-shadow-lg/25 hover:bg-neutral-200 w-24 h-10 rounded-xl text-lg font-semibold">
-                + New
+                <FontAwesomeIcon icon="fa-solid fa-plus" /> New
             </button>
         </div>
     )
