0c0deaa0d621b981f6f97c01309b826fb3d5a20c
BertAtibagos <atibagos.robert2300@gmail.com>
2026-01-21 16:52:44 +0800

    Added a table for displaying files

--- src/components/menupanel.jsx
+++ src/components/menupanel.jsx
@@ -1,4 +1,5 @@
 import SearchBar from "./searchbar.jsx"
+import InnerMenu from "./innermenu.jsx"
 
 const DirectoryLabel = (props) => {
     const { directory, classname } = props;
@@ -11,9 +12,21 @@ export default function MenuPanel(){
             <SearchBar/>
             <DirectoryLabel 
                     directory="Main Folder" 
-                    classname=" font-bold px-2"
+                    classname="font-bold px-2"
                 />
+            <table className="table-auto w-full mt-5">
+                <thead className="border-b-2 border-gray-400 text-left">
+                <tr>
+                    <th>File Name</th>
+                    <th>Date and Time</th>
+                    <th>Owner</th>
+                    <th>Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                <InnerMenu/>
+            </tbody>
+            </table>
         </div>
-                
     );
 }
